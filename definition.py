fbbff1f262ff61b542ff6bcbd72240f56ce7c4eb
User <kubo.holubek@gmail.com>
2024-03-19 13:31:53 +0100

    Definition funcionlality and adding data.csv

---
+++ definition.py
@@ -0,0 +1,16 @@
+import pandas
+import pandas as pd
+
+
+class Definition:
+
+    def __init__(self, term):
+        self.term = term
+
+    def get(self):
+        df = pandas.read_csv("data.csv")
+        return tuple(df.loc[df["word"] == str(self.term)]["definition"])
+
+
+d = Definition(term="sun")
+print(d.get())
