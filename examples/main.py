2983e14bc37591819cdb595c4d713593800ea9ca
User <kubo.holubek@gmail.com>
2024-03-21 10:41:39 +0100

    for sure commit

---
+++ examples/main.py
@@ -0,0 +1,20 @@
+import justpy as jp
+
+
+@jp.SetRoute('/home')
+def home():
+    wp = jp.WebPage()
+    div = jp.Div(a=wp, classes='bg-gray-200 h-screen')
+    jp.Input(a=div, placeholder='Enter first value',
+             classes='form-input')
+    jp.Input(a=div,placeholder='Enter second value',
+             classes='form-input')
+    jp.Div(a=div, text='Result goes here.', classes='text-gray-600')
+    jp.Button(a=div, text='Calculate',
+              classes='border border-blue-500 m-2 py-2 px-4 rounded '
+                      'text-blue-600 hover:bg-red-500 hover:text-white')
+    return wp
+
+# jp.Route('/home', home)
+
+jp.justpy()
