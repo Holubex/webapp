10ed5b1dd460015b12d93b204b1eb09e628899ca
User <kubo.holubek@gmail.com>
2024-03-22 12:27:24 +0100

    implementing get definition

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import justpy as jp
 
 from webapp1.home import Home
 from webapp1.about import About
+from webapp1.dictionary import Dictionary
 
 jp.Route(Home.path, Home.serve)
 jp.Route(About.path, About.serve)
-
+jp.Route(Dictionary.path, Dictionary.serve)
 
 
 jp.justpy(port=8001)
