c55e3bd3b9171f12ef69d048248552a206d700cd
User <kubo.holubek@gmail.com>
2024-04-06 18:47:58 +0200

    Adding layout

--- webapp1/home.py
+++ webapp1/home.py
@@ -3,15 +3,29 @@ import justpy as jp
 class Home:
     path = '/'
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+
+        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
+        header = jp.QHeader(a=layout)
+        toolbar = jp.QToolbar(a=header)
+
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_mode='left', bordered=True)
+        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon='menu',
+                click=cls.move_drawer, drawer=drawer)
+        jp.QToolbarTitle(a=toolbar,text='Instant Dictionary')
+
+        div = jp.Div(a=layout, classes="bg-white-200 h-screen")
         jp.Div(a=div, text='This is the Home Page!', classes='text-4xl m-2')
         jp.Div(a=div, text='''
-                hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-                hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-                hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-                hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-                hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-                ''', classes='text-lg')
+                Aplikacia Dzefkoviteho typu
+                ''', classes='flex justify-center items-center h-screen')
         return wp
+
+    @staticmethod
+    def move_drawer(widget, msg):
+        if widget.drawer.value:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
